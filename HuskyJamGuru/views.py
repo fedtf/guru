4d677b76c133bcebed47cfb4cb6b6434597aec49
Matvey-Kuk <motakuk@gmail.com>
2015-09-07 20:48:13 +0300

    Отображение маелстоунов и задач

--- HuskyJamGuru/views.py
+++ HuskyJamGuru/views.py
@@ -15,19 +15,29 @@ class Dashboard(TemplateView):
                 get_gitlab(self.request).get("http://185.22.60.142:8889/api/v3/projects").content.decode("utf-8")
             )
 
-            issues = json.loads(
-                    get_gitlab(self.request).get("http://185.22.60.142:8889/api/v3/issues").content.decode("utf-8")
+            issues_closed = json.loads(
+                get_gitlab(self.request).get(
+                    "http://185.22.60.142:8889/api/v3/issues?state=closed"
+                ).content.decode("utf-8")
             )
 
-            # print(issues)
+            issues_opened = json.loads(
+                get_gitlab(self.request).get(
+                    "http://185.22.60.142:8889/api/v3/issues?state=opened"
+                ).content.decode("utf-8")
+            )
 
-            for project in projects:
-                print(project["name"])
-                for issue in issues:
-                    if issue["project_id"] == project["id"]:
-                        print(issue)
+            milestones = json.loads(
+                get_gitlab(self.request).get(
+                    "http://185.22.60.142:8889/api/v3/projects/7/milestones"
+                ).content.decode("utf-8")
+            )
 
-            # context['issues'] = issues
+            context['issues_opened'] = issues_opened
+            context['issues_closed'] = issues_closed
+            context['projects'] = projects
+            context['milestones'] = milestones
+
         return context
 
 
