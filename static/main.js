357a86b4f6c9c008a3f666d9d398e89888b954b1
Matvey-Kuk <motakuk@gmail.com>
2015-09-07 20:03:42 +0300

    Верстка маелстоунов

--- static/main.js
+++ static/main.js
@@ -1,9 +1,11 @@
 $(function() {
-	
-	$('.dropable').sortable({
-		connectWith: ".dropable",
-		revert: true,
-		cursor: 'move'
-	});
-	
+    $('.dropable').sortable({
+        connectWith: ".dropable",
+        revert: true,
+        cursor: 'move',
+        activeClass: "ui-state-hover",
+        receive: function(event, ui){
+            console.log(event);
+        }
+    });
 });
