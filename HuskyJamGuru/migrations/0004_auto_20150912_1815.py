bc2a74dc1dee2cfab94414099776b0e55b383b49
Matvey-Kuk <motakuk@gmail.com>
2015-09-13 00:13:10 +0300

    migrations

--- HuskyJamGuru/migrations/0004_auto_20150912_1815.py
+++ HuskyJamGuru/migrations/0004_auto_20150912_1815.py
@@ -25,7 +25,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='project',
             name='id',
-            field=models.AutoField(auto_created=True, primary_key=True, default=1, serialize=False, verbose_name='ID'),
+            field=models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
             preserve_default=False,
         ),
         migrations.AlterField(
