b6bad36cb1b0c06b80b72ea1931948415cefc5b2
Matvey-Kuk <motakuk@gmail.com>
2015-09-16 17:16:50 +0300

    Больше воркеров богу воркеров

--- Deploy/start_in_docker.sh
+++ Deploy/start_in_docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 sudo /etc/init.d/nginx start;
-uwsgi --socket /tmp/uwsgi.sock --module Project.wsgi --chmod-socket=777
+uwsgi --socket /tmp/uwsgi.sock --module Project.wsgi --chmod-socket=777 --processes=10
