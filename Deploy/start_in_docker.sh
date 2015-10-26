eee4d204e58cf59844b42961b678c113a6081d40
fedtf <fed.tf@yandex.ru>
2015-10-26 12:21:39 +0500

    celery log #12

--- Deploy/start_in_docker.sh
+++ Deploy/start_in_docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-python ../manage.py celery worker --setttings Project.production_settings &
+python ../manage.py celery worker --logfile ../celery-log.txt --setttings Project.production_settings &
 sudo /etc/init.d/nginx start;
 uwsgi --socket /tmp/uwsgi.sock --module Project.wsgi --chmod-socket=777 --processes=10
