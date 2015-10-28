bce1687f75e6453cd929348749d66a656aa18bed
fedtf <fed.tf@yandex.ru>
2015-10-28 12:01:02 +0500

    changed celery start command #12

--- Deploy/start_in_docker.sh
+++ Deploy/start_in_docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-DJANGO_SETTINGS_MODULE='Project.production_settings' celery -A Project worker -l info 2>&1 &
+DJANGO_SETTINGS_MODULE='Project.production_settings' python manage.py celery worker --logfile django.log -l info 2>&1 &
 sudo /etc/init.d/nginx start;
 uwsgi --socket /tmp/uwsgi.sock --module Project.wsgi --chmod-socket=777 --processes=10
