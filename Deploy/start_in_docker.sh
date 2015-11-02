1f183a361ec64de538965eca39d9ba1d08c38ff1
Matvey-Kuk <motakuk@gmail.com>
2015-11-02 15:57:35 +0300

    Test Celery

--- Deploy/start_in_docker.sh
+++ Deploy/start_in_docker.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 sudo /etc/init.d/nginx start;
+
+DJANGO_SETTINGS_MODULE='Project.production_settings' celery -A Project worker -l info 2>&1 &
+
 uwsgi --socket /tmp/uwsgi.sock --module Project.wsgi --chmod-socket=777 --processes=10
