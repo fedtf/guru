b170ef15d8bf51370884144549d14e6a250b97d8
fedtf <fed.tf@yandex.ru>
2015-11-09 22:53:36 +0500

    delete postfix server

--- Deploy/start_in_docker.sh
+++ Deploy/start_in_docker.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 sudo /etc/init.d/nginx start;
-sudo /etc/init.d/postfix start;
 
 C_FORCE_ROOT=1 DJANGO_SETTINGS_MODULE='Project.production_settings' celery -A Project worker -l info --logfile celery-log.txt 2>&1 &
 
